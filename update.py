cf62d0cef2720b0a40b037f2f6f001cbbe212a05
hermens16 <103794291+hermens16@users.noreply.github.com>
2026-02-23 03:22:20 -0300

    Atualizar o update.py

--- update.py
+++ update.py
@@ -8,14 +8,22 @@ with open("channels.json") as f:
 os.makedirs("live", exist_ok=True)
 
 for name, url in channels.items():
+    print(f"Processando: {name}")
+
     result = subprocess.run(
         ["yt-dlp", "-f", "best[ext=m3u8]", "-g", url],
         capture_output=True,
         text=True
     )
 
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+
     hls_url = result.stdout.strip()
 
     if hls_url:
         with open(f"live/{name}.m3u8", "w") as f:
             f.write(hls_url)
+        print("Arquivo criado.")
+    else:
+        print("Nenhum link gerado.")
