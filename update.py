b4022027b735e44379178a13b7301175adb46454
hermens16 <103794291+hermens16@users.noreply.github.com>
2026-02-22 23:43:52 -0300

    Criar update.py

---
+++ update.py
@@ -0,0 +1,21 @@
+import subprocess
+import json
+import os
+
+with open("channels.json") as f:
+    channels = json.load(f)
+
+os.makedirs("live", exist_ok=True)
+
+for name, url in channels.items():
+    result = subprocess.run(
+        ["yt-dlp", "-g", url],
+        capture_output=True,
+        text=True
+    )
+
+    hls_url = result.stdout.strip()
+
+    if hls_url:
+        with open(f"live/{name}.m3u8", "w") as f:
+            f.write(hls_url)
