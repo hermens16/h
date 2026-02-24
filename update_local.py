d40f3e527dd3f94c1ea52f0c484f5d3838c9118c
hermens16 <hermens16@hotmail.com>
2026-02-24 11:15:37 -0300

    auto update lives

--- update_local.py
+++ update_local.py
@@ -1,18 +1,39 @@
 import subprocess
+import os
 
-url = "https://www.youtube.com/watch?v=GENH9mWlvb4"
+# Lê lista de canais
+with open("canais.txt", "r", encoding="utf-8") as file:
+    linhas = file.readlines()
 
-result = subprocess.run(
-    ["yt-dlp.exe", "-g", url],
-    capture_output=True,
-    text=True
-)
+for linha in linhas:
+    linha = linha.strip()
 
-hls_url = result.stdout.strip()
+    if not linha or "|" not in linha:
+        continue
 
-if hls_url:
-    with open("lives/livro24h.m3u8", "w") as f:
-        f.write(hls_url)
-    print("Arquivo atualizado com sucesso.")
-else:
-    print("Não conseguiu gerar link.")
+    nome, url = linha.split("|", 1)
+
+    print(f"Atualizando {nome}...")
+
+    result = subprocess.run(
+        ["yt-dlp.exe", "-g", url],
+        capture_output=True,
+        text=True
+    )
+
+    hls_url = result.stdout.strip()
+
+    if hls_url:
+        with open(f"lives/{nome}.m3u8", "w", encoding="utf-8") as f:
+            f.write(hls_url)
+
+        print(f"{nome} atualizado com sucesso.")
+    else:
+        print(f"Erro ao atualizar {nome}")
+
+# Commit automático
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "auto update lives"])
+subprocess.run(["git", "push"])
+
+print("Processo finalizado.")
